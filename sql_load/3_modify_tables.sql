35558732b93d1432e61489ac6915ca11331b5b12
Ankush Santra <90343148+Ankush-Santra@users.noreply.github.com>
2025-01-18 22:54:36 +0530

    Update 3_modify_tables.sql

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,17 +1,17 @@
-/* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
-Database Load Issues (follow if receiving permission denied when running SQL code below)
+/* 
+If you encounter a permissions issue with the following error:
 
-NOTE: If you are having issues with permissions. And you get error: 
+"could not open file "[your file path]\job_postings_fact.csv" for reading: Permission denied."
 
-'could not open file "[your file path]\job_postings_fact.csv" for reading: Permission denied.'
-
-1. Open pgAdmin
-2. In Object Explorer (left-hand pane), navigate to `sql_course` database
-3. Right-click `sql_course` and select `PSQL Tool`
-    - This opens a terminal window to write the following code
-4. Get the absolute file path of your csv files
-    1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
-5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
+Follow these steps to resolve it:
+1. Open pgAdmin.
+2. In the Object Explorer (left-hand pane), navigate to the sql_course database.
+3. Right-click on sql_course and select PSQL Tool.
+    - This will open a terminal window where you can execute SQL commands.
+4. Locate the absolute file path of your CSV file:
+    - In VS Code, right-click the CSV file and select Copy Path to copy its full path.
+5. In the PSQL Tool, run the appropriate command using the correct file path.
+Make sure to replace [your file path] with the absolute path of your CSV file.
 
 \copy company_dim FROM '[Insert File Path]/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
@@ -22,8 +22,6 @@ NOTE: If you are having issues with permissions. And you get error:
 \copy skills_job_dim FROM '[Insert File Path]/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 */
-
--- NOTE: This has been updated from the video to fix issues with encoding
 
 COPY company_dim
 FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\company_dim.csv'
